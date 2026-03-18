58ec77e18104ef2a4272bd4bcd0ce83a5287249c
oliverroyal0 <oliverroyal0@gmail.com>
2026-03-17 23:42:37 -0700

    Fix recharts react-is dependency for Vite 8 build

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,16 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+  resolve: {
+    alias: {},
+  },
+  optimizeDeps: {
+    include: ["react-is"],
+  },
 })
